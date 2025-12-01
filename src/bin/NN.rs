f85e24bdb6528cb94d4e0a26e53ecbd41fc1b078
abdo <abdelrahmanorigin@gmail.com>
2025-12-01 13:41:29 +0200

    remove // TOODs

--- src/bin/NN.rs
+++ src/bin/NN.rs
@@ -1,16 +1,16 @@
+use adv_code_2025::*;
 use anyhow::*;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
-use adv_code_2025::*;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-const DAY: &str = "NN"; // TODO: Fill the day
+const DAY: &str = "NN";
 const INPUT_FILE: &str = concatcp!("input/", DAY, ".txt");
 
 const TEST: &str = "\
 <TEST-INPUT>
-"; // TODO: Add the test input
+";
 
 fn main() -> Result<()> {
     start_day(DAY);
@@ -19,13 +19,11 @@ fn main() -> Result<()> {
     println!("=== Part 1 ===");
 
     fn part1<R: BufRead>(reader: R) -> Result<usize> {
-        // TODO: Solve Part 1 of the puzzle
-        let answer = reader.lines().flatten().count();
-        Ok(answer)
+        todo!()
     }
 
-    // TODO: Set the expected answer for the test input
     assert_eq!(0, part1(BufReader::new(TEST.as_bytes()))?);
+    println!("=== Part 1 sample test input end ===");
 
     let input_file = BufReader::new(File::open(INPUT_FILE)?);
     let result = time_snippet!(part1(input_file)?);
