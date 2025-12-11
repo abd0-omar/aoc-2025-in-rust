a4a656d5663d37f4807459511756df9b2c6e30b1
abdo <abdelrahmanorigin@gmail.com>
2025-12-11 08:31:43 +0200

    day11 part2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -22,6 +22,21 @@ ggg: out
 hhh: ccc fff iii
 iii: out";
 
+const TEST_PART2: &str = "\
+svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out";
+
 fn main() -> Result<()> {
     start_day(DAY);
 
@@ -80,19 +95,62 @@ fn main() -> Result<()> {
     //endregion
 
     //region Part 2
-    // println!("\n=== Part 2 ===");
-    //
-    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
-    // let result = 0;
-    // Ok(result)
-    // }
-    //
-    // assert_eq!(0, part2(BufReader::new(TEST.as_bytes()))?);
-    // println!("=== Part 2 sample end ===");
-    //
-    // let input_file = BufReader::new(File::open(INPUT_FILE)?);
-    // let result = time_snippet!(part2(input_file)?);
-    // println!("Result = {}", result);
+    println!("\n=== Part 2 ===");
+
+    fn dfs_part2<'a>(
+        cables: &'a &HashMap<String, Vec<String>>,
+        cur_input: &'a str,
+        mut fft: bool,
+        mut dac: bool,
+        memo: &mut HashMap<(&'a str, bool, bool), usize>,
+    ) -> usize {
+        if cur_input == "fft" {
+            fft = true;
+        }
+
+        if cur_input == "dac" {
+            dac = true;
+        }
+
+        if cur_input == "out" {
+            return if dac && fft { 1 } else { 0 };
+        }
+
+        if let Some(&cached_result) = memo.get(&(cur_input, fft, dac)) {
+            return cached_result;
+        }
+
+        let mut result = 0;
+        if let Some(neighbors) = cables.get(cur_input) {
+            for neighbor in neighbors {
+                result += dfs_part2(cables, neighbor, fft, dac, memo);
+            }
+        }
+
+        memo.insert((cur_input, fft, dac), result);
+
+        result
+    }
+
+    fn part2<R: BufRead>(reader: R) -> Result<usize> {
+        // simple dfs
+        let mut result = 0;
+
+        let cables = parse(reader);
+
+        let mut memo = HashMap::new();
+
+        result += dfs_part2(&&cables, "svr", false, false, &mut memo);
+
+        Ok(result)
+    }
+
+    assert_eq!(2, part2(BufReader::new(TEST_PART2.as_bytes()))?);
+    println!("=== Part 2 sample end ===");
+
+    let input_file = BufReader::new(File::open(INPUT_FILE)?);
+    let result = time_snippet!(part2(input_file)?);
+    println!("Result = {}", result);
     //endregion
 
     Ok(())
