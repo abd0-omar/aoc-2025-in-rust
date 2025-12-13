8731f5d75b49ad81c1896594a2ec2b249b585257
abdo <abdelrahmanorigin@gmail.com>
2025-12-13 04:39:45 +0200

    day10 part2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -2,6 +2,7 @@ use adv_code_2025::*;
 use anyhow::*;
 use code_timing_macros::time_snippet;
 use const_format::concatcp;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::result::Result::Ok;
@@ -168,25 +169,116 @@ fn main() -> Result<()> {
     println!("\n=== Part 2 ===");
 
     // TLE
+    // fn part2<R: BufRead>(reader: R) -> Result<usize> {
+    //     // ignore light indicators requirements for part2
+    //     let mut result = 0;
+
+    //     let (_, buttons, joltage) = parse(reader);
+
+    //     for (mut cur_machine_joltage, cur_machine_buttons) in joltage.into_iter().zip(buttons) {
+    //         let mut cur_result = usize::MAX;
+    //         backtrack_part2(
+    //             &mut cur_machine_joltage,
+    //             &cur_machine_buttons,
+    //             0,
+    //             0,
+    //             &mut cur_result,
+    //         );
+    //         result += cur_result
+    //     }
+
+    //     Ok(result)
+    // }
+
+    // solution based on this https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
     fn part2<R: BufRead>(reader: R) -> Result<usize> {
-        // ignore light indicators requirements for part2
+        let (_, buttons, joltage) = parse(reader);
         let mut result = 0;
 
-        let (_, buttons, joltage) = parse(reader);
+        for (cur_machine_joltage, cur_machine_buttons) in joltage.into_iter().zip(buttons) {
+            let mut memo = HashMap::new();
 
-        for (mut cur_machine_joltage, cur_machine_buttons) in joltage.into_iter().zip(buttons) {
-            let mut cur_result = usize::MAX;
-            backtrack_part2(
-                &mut cur_machine_joltage,
-                &cur_machine_buttons,
-                0,
-                0,
-                &mut cur_result,
-            );
-            result += cur_result
+            let presses =
+                solve_machine_recursive(&cur_machine_joltage, &cur_machine_buttons, &mut memo);
+
+            result += presses;
         }
 
         Ok(result)
+    }
+
+    fn solve_machine_recursive(
+        joltages: &Vec<usize>,
+        buttons: &Vec<Vec<usize>>,
+        memo: &mut HashMap<Vec<usize>, usize>,
+    ) -> usize {
+        if joltages.iter().all(|&j| j == 0) {
+            return 0;
+        }
+
+        if let Some(&res) = memo.get(joltages) {
+            return res;
+        }
+
+        let mut current_parity_mask = 0u64;
+        for (i, &val) in joltages.iter().enumerate() {
+            if val % 2 != 0 {
+                current_parity_mask |= 1 << i;
+            }
+        }
+
+        let num_buttons = buttons.len();
+        let mut min_presses = usize::MAX;
+
+        for mask in 0..(1 << num_buttons) {
+            let mut subset_xor_effect = 0u64;
+            let mut subset_size = 0;
+
+            for i in 0..num_buttons {
+                if (mask >> i) & 1 == 1 {
+                    subset_size += 1;
+                    for &affected_idx in &buttons[i] {
+                        subset_xor_effect ^= 1 << affected_idx;
+                    }
+                }
+            }
+
+            if subset_xor_effect == current_parity_mask {
+                let mut next_joltages = joltages.clone();
+                let mut possible = true;
+
+                for i in 0..num_buttons {
+                    if (mask >> i) & 1 == 1 {
+                        for &affected_idx in &buttons[i] {
+                            if next_joltages[affected_idx] > 0 {
+                                next_joltages[affected_idx] -= 1;
+                            } else {
+                                possible = false;
+                                break;
+                            }
+                        }
+                    }
+                    if !possible {
+                        break;
+                    }
+                }
+
+                if possible {
+                    for val in &mut next_joltages {
+                        *val /= 2;
+                    }
+
+                    let res = solve_machine_recursive(&next_joltages, buttons, memo);
+
+                    if res != usize::MAX {
+                        min_presses = min_presses.min(subset_size + 2 * res);
+                    }
+                }
+            }
+        }
+
+        memo.insert(joltages.clone(), min_presses);
+        min_presses
     }
 
     fn backtrack_part2(
